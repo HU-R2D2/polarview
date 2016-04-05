c7f310d97b8fc78ca5f8863d7492a18afb5034d1
Aydin Biber <biber.aydin@gmail.com>
2016-04-05 13:06:01 +0200

    Added setters to DistanceReading
    Added setters so the Distance and ResultType can be set.

--- source/include/DistanceReading.hpp
+++ source/include/DistanceReading.hpp
@@ -11,12 +11,45 @@ enum class ResultType {
 };
 
 class DistanceReading {
+	
 private:
 	Distance distance;
 	ResultType type;
+	
 public:
+	//! DistanceReading constructor
 	DistanceReading(Distance dist, ResultType type);
+	
+	//! set_result_type
+	/*!
+	 Setter for the ResultType object
+	 \param ResultType
+	 \return
+	 */
+	void set_result_type(ResultType t);
+	
+	//! get_result_type
+	/*!
+	 Getter for the ResultType object
+	 \param
+	 \return ResultType type
+	 */
 	ResultType get_result_type();
+	
+	//! set_distance
+	/*!
+	 Setter for the Distance object
+	 \param Distance
+	 \return
+	 */
+	void set_distance(Distance d);
+	
+	//! get_distance
+	/*!
+	 Getter for the Distance object
+	 \param
+	 \return Distance distance
+	*/
 	Distance get_distance();
 };
 
--- source/src/DistanceReading.cpp
+++ source/src/DistanceReading.cpp
@@ -1,11 +1,21 @@
 #include "../include/DistanceReading.hpp"
 
-DistanceReading::DistanceReading(Distance dist, ResultType type) : distance(dist), type(type) {
+DistanceReading::DistanceReading(Distance dist, ResultType type) :
+	distance(dist), type(type)
+{
 	
+}
+
+void DistanceReading::set_result_type(ResultType t) {
+	this->type = t;
 }
 
 ResultType DistanceReading::get_result_type() {
 	return this->type;
+}
+
+void DistanceReading::set_distance(Distance d) {
+	this->distance = d;
 }
 
 Distance DistanceReading::get_distance() {
