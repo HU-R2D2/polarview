63f6b41832ac45b3ae96673595b1af0c00aaefcd
Aydin Biber <biber.aydin@gmail.com>
2016-04-05 10:39:49 +0200

    Small change in test.cpp for a simpler test

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 #include "../source/include/DistanceReading.hpp"
 
-
+/*
 TEST(GetResultType, GetResultType) {
 	ResultType didnt = ResultType::DIDNT_CHECK;
 	ResultType range = ResultType::OUT_OF_RANGE;
@@ -9,7 +9,7 @@ TEST(GetResultType, GetResultType) {
 	
 	Distance dist;
 	
-	DistanceReading a(DistanceReading(dist, didnt));
+	DistanceReading a = DistanceReading(dist, didnt);
 	DistanceReading b(DistanceReading(dist, range));
 	DistanceReading c(DistanceReading(dist, check));
 	
@@ -17,12 +17,12 @@ TEST(GetResultType, GetResultType) {
 	EXPECT_EQ(b.get_result_type(), ResultType::OUT_OF_RANGE);
 	EXPECT_EQ(c.get_result_type(), ResultType::CHECKED);
 }
+*/
 
-/*
+
 TEST(GetDistance, GetDistance) {
 	Distance dist;
-	DistanceReading a(DistanceReading(dist, ResultType::DIDNT_CHECK));
+	DistanceReading a = DistanceReading(dist, ResultType::DIDNT_CHECK);
 	
 	EXPECT_EQ(a.get_distance(), dist);
 }
-*/
