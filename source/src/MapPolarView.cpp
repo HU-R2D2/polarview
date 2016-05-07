26d5493b08575d5a50803d12f903ad0c84375994
Christiaan van den Berg <himitsuyaluzan@gmail.com>
2016-05-07 11:04:04 +0200

    Worked the fagan into the code
    Changed tabs to spaces in DistanceReading and MapPolarView.cpp
    Added better doxygen documentation for MapPolarView.hpp and DistanceReading.hpp

--- source/src/MapPolarView.cpp
+++ source/src/MapPolarView.cpp
@@ -1,11 +1,11 @@
 //! \addtogroup 0012 Polar view
 //! \brief A polar view is a 360 degree view around a point.
-//!    
+//!
 //! A polar view is what a distance meter sees if it is turned around to
 //! look in all directions: for each direction (angle) a distance to the
 //! nearest object (for now we ‘think’ in 2D only). Such a view could be
 //! delivered from a Lidar, or from a robot, with an UltraSonic distance
-//! sensor, by turning around its axis.         
+//! sensor, by turning around its axis.
 //!
 //! \file   MapPolarView.cpp
 //! \author Aydin Biber - 1666849, Jasper van hulst - 1660498,
@@ -14,35 +14,35 @@
 //! \date   Last Modified: 25-04-2016
 //!
 //!
-//! \copyright Copyright © 2016, HU University of Applied Sciences Utrecht. 
+//! \copyright Copyright © 2016, HU University of Applied Sciences Utrecht.
 //! All rights reserved.
-//! 
+//!
 //! License: newBSD
 //!
-//! Redistribution and use in source and binary forms, 
-//! with or without modification, are permitted provided that 
+//! Redistribution and use in source and binary forms,
+//! with or without modification, are permitted provided that
 //! the following conditions are met:
-//! - Redistributions of source code must retain the above copyright notice, 
+//! - Redistributions of source code must retain the above copyright notice,
 //!   this list of conditions and the following disclaimer.
-//! - Redistributions in binary form must reproduce the above copyright notice, 
-//!   this list of conditions and the following disclaimer in the documentation 
+//! - Redistributions in binary form must reproduce the above copyright notice,
+//!   this list of conditions and the following disclaimer in the documentation
 //!   and/or other materials provided with the distribution.
-//! - Neither the name of the HU University of Applied Sciences Utrecht 
-//!   nor the names of its contributors may be used to endorse or promote 
-//!   products derived from this software without specific prior written 
+//! - Neither the name of the HU University of Applied Sciences Utrecht
+//!   nor the names of its contributors may be used to endorse or promote
+//!   products derived from this software without specific prior written
 //!   permission.
 //!
 //! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-//! BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
-//! AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//! "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+//! BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+//! AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
 //! IN NO EVENT SHALL THE HU UNIVERSITY OF APPLIED SCIENCES UTRECHT
 //! BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
-//! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-//! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-//! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
-//! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//! PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+//! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+//! WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+//! OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
 //! EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // ~< HEADER_VERSION 2016 04 12 >~
 
@@ -50,37 +50,35 @@
 #include "../include/DistanceReading.hpp"
 
 MapPolarView::MapPolarView() {
-for(int i = 0; i < 360; i++) {
-    readings.insert(std::pair<r2d2::Angle,
-        DistanceReading>(
-            r2d2::Angle(i * r2d2::Angle::deg),
-                DistanceReading(
+    for(int i = 0; i < 360; i++) {
+        readings.insert(std::pair<r2d2::Angle,
+            DistanceReading>(
+                r2d2::Angle(i * r2d2::Angle::deg),
                     DistanceReading(
-                        r2d2::Length(),
-                        DistanceReading::ResultType::DIDNT_CHECK
+                        DistanceReading(
+                            r2d2::Length(),
+                            DistanceReading::ResultType::DIDNT_CHECK
+                        )
                     )
-                )
             )
         );
     }
 }
 
-//Temporary implemented to check 2 Angles with a offset.
-//Will be replaced by ADT comparator lateron. (AKA Never)
 bool angle_range(r2d2::Angle angle1, r2d2::Angle angle2,
                  double offset = 0.0001){
-	return ((angle1  - (offset * r2d2::Angle::rad)) < angle2) && 
+    return ((angle1  - (offset * r2d2::Angle::rad)) < angle2) &&
     (angle2 < (angle1  + (offset * r2d2::Angle::rad)));
 }
 
 PolarView& MapPolarView::collapse(){
-	std::vector<r2d2::Angle> keyValues;
+    std::vector<r2d2::Angle> keyValues;
     r2d2::Angle circle(M_PI*2 * r2d2::Angle::rad);
-	for(const auto & angle: readings){
-		if(r2d2::Angle(angle.first) > circle){
-			keyValues.push_back(angle.first);
-		}
-	}
+    for(const auto & angle: readings){
+        if(r2d2::Angle(angle.first) > circle){
+            keyValues.push_back(angle.first);
+        }
+    }
     for(const auto & extraAngle: keyValues){
         r2d2::Angle adjustedAngle(extraAngle);
         while(adjustedAngle > circle){
@@ -99,7 +97,7 @@ PolarView& MapPolarView::collapse(){
             }
         }
     }
-	return (*this);
+    return (*this);
 }
 
 void MapPolarView::rotate(r2d2::Angle angle){
@@ -113,7 +111,7 @@ void MapPolarView::rotate(r2d2::Angle angle){
              *r2d2::Angle::deg))));
     }
     for(int i = readings.size()-1; i > 0+degrees; i--){
-        readings.at(r2d2::Angle(i*r2d2::Angle::deg)) = 
+        readings.at(r2d2::Angle(i*r2d2::Angle::deg)) =
         readings.at(r2d2::Angle((i-degrees)*r2d2::Angle::deg));
     }
     for(int i = 0; i < buffer.size(); i++){
@@ -123,20 +121,20 @@ void MapPolarView::rotate(r2d2::Angle angle){
 }
 
 double MapPolarView::match(PolarView& v) {
-    double c = 0;
+    double count = 0;
     double len1, len2;
     double offset = 0.0001; // Precise value to measure by
     for(int i = 0; i < 360; i++) {
-        len1 = (readings.at(r2d2::Angle(i*r2d2::Angle::deg)).get_length() / 
+        len1 = (readings.at(r2d2::Angle(i*r2d2::Angle::deg)).get_length() /
             r2d2::Length::METER);
         len2 = (v.get_distances().at(r2d2::Angle(i*r2d2::Angle::deg))
             .get_length() / r2d2::Length::METER);
 
         if(((len1  - offset) < len2) && (len2 < (len1  + offset))) {
-            c++;
+            count++;
         }
     }
-    return (c/360)*100;
+    return (count/360)*100;
 }
 
 std::tuple<r2d2::Angle, double> MapPolarView::find_best_match(PolarView& v){
@@ -203,7 +201,7 @@ PolarView& MapPolarView::operator+=(PolarView& v) {
 
 PolarView& MapPolarView::operator+(PolarView& v) {
     MapPolarView retPV = (*this);
-    MapPolarView& copyPV = 
+    MapPolarView& copyPV =
     *(new MapPolarView(static_cast<MapPolarView&>(retPV += v)));
     return copyPV;
 }
@@ -215,16 +213,16 @@ void MapPolarView::add_distancereading(r2d2::Angle angle, r2d2::Length len,
         readings.at(angle).set_length(len);
         readings.at(angle).set_result_type(type);
     }
-	else{
-		readings.insert(std::pair<r2d2::Angle, DistanceReading>(angle, 
+    else{
+        readings.insert(std::pair<r2d2::Angle, DistanceReading>(angle,
                         DistanceReading(len, type)));
-	}
+    }
 }
 void MapPolarView::add_distancereading(r2d2::Angle angle, DistanceReading dist){
     if(readings.count(angle) > 0){
         readings.at(angle) = dist;
     }
-	else{
-		readings.insert(std::pair<r2d2::Angle, DistanceReading>(angle, dist));
-	}
+    else{
+        readings.insert(std::pair<r2d2::Angle, DistanceReading>(angle, dist));
+    }
 }
